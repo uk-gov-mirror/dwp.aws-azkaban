02bec3d7427e18a6fd269c39aceadf070d23654c
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-03-26 09:31:58 +0000

    Jmx exporter (#54)
    * Add jmx-exporter sidecar
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add JMX exporter, make get-deps
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * newline
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add sidecar
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * pin image versions
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * update to latest
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * ver bump, use underscores
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * ver bump
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Correct permissions and paths
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * ver bump, output sg, correct port
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * enable metrics
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * enable jmxremote
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add specific Azkaban rules, target executor jmxremote port
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * output webserver sg
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * fmt
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * local apply
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * change config location
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add sidecar to webserver, create ocnfig for sidecar
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * dedupe s3 objects
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * enable jmxremote
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Allow getting of config
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add 2 min delay
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * use jmxUrl, 2 min delay for both
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * rm rules for webserver, delay is server not collect so reverted
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * stop executor recreating tables
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add Azkaban rules now we're seeing life
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * rm executor based exporter
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * rm jmxremote on executor
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Don't be square
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add ECS discovery label
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * consistent code, adding rollout deployment to webserver
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * rm hashed code
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * add default assume role
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * dupe var, allow ci to assume role, use TF exit codes to run faster
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Allow PRs to run with TF skips
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * fixed zip name
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- azkaban_database_sg.tf
+++ azkaban_database_sg.tf
@@ -28,6 +28,25 @@ resource "aws_security_group_rule" "allow_azkaban_executor_ingress_azkaban_datab
   security_group_id        = aws_security_group.azkaban_database.id
   source_security_group_id = aws_security_group.azkaban_executor.id
 }
+resource "aws_security_group_rule" "allow_azkaban_external_webserver_ingress_azkaban_database" {
+  description              = "Allows azkaban external webserver to access azkaban database"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = aws_rds_cluster.azkaban_database.port
+  to_port                  = aws_rds_cluster.azkaban_database.port
+  security_group_id        = aws_security_group.azkaban_database.id
+  source_security_group_id = aws_security_group.azkaban_external_webserver.id
+}
+
+resource "aws_security_group_rule" "allow_azkaban_external_executor_ingress_azkaban_database" {
+  description              = "Allows azkaban external executor to access azkaban database"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = aws_rds_cluster.azkaban_database.port
+  to_port                  = aws_rds_cluster.azkaban_database.port
+  security_group_id        = aws_security_group.azkaban_database.id
+  source_security_group_id = aws_security_group.azkaban_external_executor.id
+}
 
 resource "aws_security_group_rule" "allow_rotate_password_lambda_ingress_azkaban_database" {
   description              = "Allows rotate password lambda to access azkaban database"
